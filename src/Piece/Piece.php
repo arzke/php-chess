f2885f8c318f8952219692ede59b48e3a7c5f0ca
Bruno Arsene <arsene.bruno@gmail.com>
2014-08-01 20:36:11 +0300

    Create the Piece abstract class

---
+++ src/Piece/Piece.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Chess\Piece;
+
+
+abstract class Piece {
+
+    /**
+     * @var int
+     */
+    private $type;
+
+    /**
+     * @var string
+     */
+    private $color;
+
+    /**
+     * @return int
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return string
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function __construct($type, $color)
+    {
+        $this->type = $type;
+        $this->color = $color;
+    }
+} 
