ff46842b6dc62c823e448d082fc53e2e7ad39bcc
Bruno Arsene <arsene.bruno@gmail.com>
2014-08-01 20:46:07 +0300

    Create the King class

---
+++ src/Piece/Type/King.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Chess\Piece\Type;
+
+
+use Chess\Piece\Piece;
+
+class King extends Piece {
+
+    public function __construct($color)
+    {
+        parent::__construct(PieceType::KING, $color);
+    }
+} 
