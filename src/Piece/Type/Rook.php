fadc97c46a82f58074eb73db8599bdef983eeb7d
Bruno Arsene <arsene.bruno@gmail.com>
2014-08-01 20:49:28 +0300

    Create the Rook class

---
+++ src/Piece/Type/Rook.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Chess\Piece\Type;
+
+
+use Chess\Piece\Piece;
+
+class Rook extends Piece {
+
+    public function __construct($color)
+    {
+        parent::__construct(PieceType::ROOK, $color);
+    }
+} 
