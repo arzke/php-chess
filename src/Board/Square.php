923a372e29ee405fdcad5bd9e80d12f113535bae
Bruno Arsene <arsene.bruno@gmail.com>
2014-08-01 21:26:56 +0300

    Add getters and setters for the piece attribute in Square

--- src/Board/Square.php
+++ src/Board/Square.php
@@ -3,12 +3,29 @@
 namespace Chess\Board;
 
 
+use Chess\Piece\Piece;
+
 class Square {
 
     /**
      * @var \Chess\Piece\Piece
      */
     private $piece;
+
+    /**
+     * @param Piece $piece
+     */
+    public function setPiece(Piece $piece){
+        $this->piece = $piece;
+    }
+
+    /**
+     * @return Piece
+     */
+    public function getPiece()
+    {
+        return $this->piece;
+    }
 
     /**
      * @return bool
