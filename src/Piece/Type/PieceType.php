b317f8e7cb1291df3be5fbe65ae7bbf6db69d961
Bruno Arsene <arsene.bruno@gmail.com>
2014-08-01 20:39:15 +0300

    Create PieceType
    This class contains all the constants defining the type of the piece:
    king, queen, bishop, knight, rook and pawn.

---
+++ src/Piece/Type/PieceType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Chess\Piece\Type;
+
+
+final class PieceType {
+
+    const KING = 0;
+
+    const QUEEN = 1;
+
+    const ROOK = 3;
+
+    const BISHOP = 4;
+
+    const KNIGHT = 5;
+
+    const PAWN = 6;
+    
+} 
