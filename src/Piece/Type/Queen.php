2300dfb5eda1126f42a6594a04c008c2f115aa2f
Bruno Arsene <arsene.bruno@gmail.com>
2014-08-01 20:48:51 +0300

    Create the Queen class

---
+++ src/Piece/Type/Queen.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Chess\Piece\Type;
+
+
+use Chess\Piece\Piece;
+
+class Queen extends Piece {
+
+    public function __construct($color)
+    {
+        parent::__construct(PieceType::QUEEN, $color);
+    }
+} 
