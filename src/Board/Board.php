6b582fa1ed80e2c0a77eb904d4bbc0fdbda0ddab
Bruno Arsene <arsene.bruno@gmail.com>
2014-08-09 14:58:13 +0300

    Create method to place a Piece on Board

--- src/Board/Board.php
+++ src/Board/Board.php
@@ -3,6 +3,7 @@
 namespace Chess\Board;
 
 
+use Chess\Piece\Piece;
 use Chess\Position\Checker\PositionChecker;
 use Chess\Position\Position;
 
@@ -16,6 +17,17 @@ class Board {
     {
         $line = array_fill(0, self::SIZE, new Square());
         $this->board = array_fill(0, self::SIZE, $line);
+    }
+
+    /**
+     * @param Piece $piece
+     * @param Position $position
+     * @return bool
+     * @throws \OutOfRangeException
+     */
+    public function setAt(Piece $piece, Position $position)
+    {
+        $this->getSquare($position)->setPiece($piece);
     }
 
     /**
