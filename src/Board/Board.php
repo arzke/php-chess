a238603663f6928c9f001ce9c97f8e75a2b7969e
Bruno Arsene <arsene.bruno@gmail.com>
2014-08-09 14:55:54 +0300

    Add a getSquare() method to Board to retrieve a square at defined position

--- src/Board/Board.php
+++ src/Board/Board.php
@@ -3,6 +3,9 @@
 namespace Chess\Board;
 
 
+use Chess\Position\Checker\PositionChecker;
+use Chess\Position\Position;
+
 class Board {
 
     const SIZE = 8;
@@ -13,6 +16,20 @@ class Board {
     {
         $line = array_fill(0, self::SIZE, new Square());
         $this->board = array_fill(0, self::SIZE, $line);
+    }
+
+    /**
+     * @param Position $position
+     * @return \Chess\Board\Square
+     * @throws \OutOfRangeException
+     */
+    public function getSquare(Position $position)
+    {
+        $positionChecker = new PositionChecker();
+        if($positionChecker->isValid($position, self::SIZE)) {
+            return $this->board[$position->getX()][$position->getY()];
+        }
+        throw new \OutOfRangeException('The given coordinates are out of range.');
     }
 
 }
