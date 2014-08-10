1beccf714fbed3f6b1aa9ccd762dbad4f6274ce2
Bruno Arsene <arsene.bruno@gmail.com>
2014-08-10 22:11:12 +0300

    Create the Placer class

---
+++ src/Piece/Placer/Placer.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Chess\Piece\Placer;
+
+
+use Chess\Board\Board;
+use Chess\Color\Color;
+use Chess\Piece\Piece;
+
+abstract class Placer implements IPlacer {
+
+    /**
+     * @var array
+     */
+    protected $positions = array();
+
+    /**
+     * @param $color
+     * @return Piece
+     */
+    abstract protected function getPiece($color);
+
+    /**
+     * @return \Chess\Position\Position[]
+     */
+    abstract protected function getWhitePiecesPositions();
+
+    /**
+     * @return \Chess\Position\Position[]
+     */
+    abstract protected function getBlackPiecesPositions();
+
+    public function __construct()
+    {
+        $this->initPositions();
+    }
+
+    private function initPositions()
+    {
+        $this->positions = array(
+            Color::WHITE => $this->getWhitePiecesPositions(),
+            Color::BLACK => $this->getBlackPiecesPositions()
+        );
+    }
+
+    /**
+     * @param Board $board
+     * @return Board
+     */
+    public function place(Board $board)
+    {
+        foreach($this->positions as $color => $positions) {
+            foreach($positions as $position) {
+                $board->setAt($this->getPiece($color), $position);
+            }
+        }
+
+        return $board;
+    }
+
+} 
