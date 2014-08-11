2178e9ba21794d6162ea2e4520aa1dff14b67c70
Bruno Arsene <arsene.bruno@gmail.com>
2014-08-11 13:32:23 +0300

    Place kings on Board in BoardDirector

--- src/Board/Builder/BoardDirector.php
+++ src/Board/Builder/BoardDirector.php
@@ -4,6 +4,7 @@ namespace Chess\Board\Builder;
 
 
 use Chess\Piece\Placer\BishopPlacer;
+use Chess\Piece\Placer\KingPlacer;
 use Chess\Piece\Placer\KnightPlacer;
 use Chess\Piece\Placer\PawnPlacer;
 use Chess\Piece\Placer\QueenPlacer;
@@ -34,6 +35,7 @@ class BoardDirector {
         $this->builder->place(new KnightPlacer());
         $this->builder->place(new BishopPlacer());
         $this->builder->place(new QueenPlacer());
+        $this->builder->place(new KingPlacer());
 
         return $this->builder->getBoard();
     }
