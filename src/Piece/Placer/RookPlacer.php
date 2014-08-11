9f7d5d1c5abeaa83fd3d34302f19ab5a7e7cc0ce
Bruno Arsene <arsene.bruno@gmail.com>
2014-08-11 13:06:15 +0300

    Create the RookPlacer to place rooks

---
+++ src/Piece/Placer/RookPlacer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Chess\Piece\Placer;
+
+
+use Chess\Piece\Type\Rook;
+use Chess\Position\Position;
+
+class RookPlacer extends Placer {
+
+    /**
+     * @param $color
+     * @return \Chess\Piece\Piece|Rook
+     */
+    protected function getPiece($color)
+    {
+        return new Rook($color);
+    }
+
+    /**
+     * @return \Chess\Position\Position[]
+     */
+    protected function getWhitePiecesPositions()
+    {
+        return array(
+            new Position(7, 0),
+            new Position(7, 7)
+        );
+    }
+
+    /**
+     * @return \Chess\Position\Position[]
+     */
+    protected function getBlackPiecesPositions()
+    {
+        return array(
+            new Position(0, 0),
+            new Position(0, 7)
+        );
+    }
+
+} 
