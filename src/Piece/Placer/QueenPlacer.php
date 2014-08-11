1558aa39669aea9408778d7e0ca68719ff6b288c
Bruno Arsene <arsene.bruno@gmail.com>
2014-08-11 13:26:34 +0300

    Create the QueenPlacer to place queens

---
+++ src/Piece/Placer/QueenPlacer.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Chess\Piece\Placer;
+
+
+use Chess\Piece\Type\Queen;
+use Chess\Position\Position;
+
+class QueenPlacer extends Placer {
+
+    /**
+     * @param $color
+     * @return \Chess\Piece\Piece|Queen
+     */
+    protected function getPiece($color)
+    {
+        return new Queen($color);
+    }
+
+    /**
+     * @return \Chess\Position\Position[]
+     */
+    protected function getWhitePiecesPositions()
+    {
+        return array(
+            new Position(7, 3)
+        );
+    }
+
+    /**
+     * @return \Chess\Position\Position[]
+     */
+    protected function getBlackPiecesPositions()
+    {
+        return array(
+            new Position(0, 3)
+        );
+    }
+
+} 
