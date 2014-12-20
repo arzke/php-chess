8a1e42abd64eecaae83f295d6a4db025afdba3d0
Bruno Arsene <arsene.bruno@gmail.com>
2014-12-20 01:13:56 +0100

    Update tests for PositionChecker

--- tests/Position/Checker/PositionCheckerTest.php
+++ tests/Position/Checker/PositionCheckerTest.php
@@ -9,12 +9,63 @@ use Chess\Position\Position;
 class PositionCheckerTest extends \PHPUnit_Framework_TestCase {
 
     /**
-     * Tests that position is invalid with an empty board
      * @group position
      * @group positionChecker
      */
     public function testIsInvalidForEmptyBoard()
     {
-        $this->assertFalse((new PositionChecker())->isValid(array(), new Position(0, 0)));
+        $this->assertFalse((new PositionChecker())->isValid($this->getEmptyBoard(), new Position(0, 0)));
     }
+
+    /**
+     * @group position
+     * @group positionChecker
+     */
+    public function testIsInvalidForEmptyBoardWithEmptyRow()
+    {
+        $this->assertFalse((new PositionChecker())->isValid($this->getEmptyBoardWithEmptyRow(), new Position(0, 0)));
+    }
+
+    /**
+     * @group position
+     * @group positionChecker
+     */
+    public function testIsInvalidWithOutOfRangePosition()
+    {
+        $this->assertFalse((new PositionChecker())->isValid($this->getOneSquareBoard(), new Position(0, 1)));
+    }
+
+    /**
+     * @group position
+     * @group positionChecker
+     */
+    public function testIsValidOnExistingPosition()
+    {
+        $this->assertTrue((new PositionChecker())->isValid($this->getOneSquareBoard(), new Position(0, 0)));
+    }
+
+    /**
+     * @return array
+     */
+    private function getEmptyBoard()
+    {
+        return array();
+    }
+
+    /**
+     * @return array
+     */
+    private function getEmptyBoardWithEmptyRow()
+    {
+        return array(array());
+    }
+
+    /**
+     * @return array
+     */
+    private function getOneSquareBoard()
+    {
+        return array(array(null));
+    }
+
 }
