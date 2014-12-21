b3d43d2ce95842fa6ff1c745e6953683a562ef0b
Bruno Arsene <arsene.bruno@gmail.com>
2014-12-21 21:27:32 +0100

    Add unit tests for Queen

---
+++ tests/Piece/Type/QueenTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace tests\Piece\Type;
+
+
+use Chess\Color\Color;
+use Chess\Piece\Type\PieceType;
+use Chess\Piece\Type\Queen;
+use PHPUnit_Framework_TestCase;
+
+class QueenTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * @group queen
+     * @group piece
+     */
+    public function testInstantiateWithNullColor()
+    {
+        $queen = new Queen(null);
+
+        $this->assertSame(PieceType::QUEEN, $queen->getType());
+        $this->assertSame(null, $queen->getColor());
+    }
+
+    /**
+     * @group queen
+     * @group piece
+     */
+    public function testInstantiateWithColor()
+    {
+        $queen = new Queen(Color::BLACK);
+
+        $this->assertSame(PieceType::QUEEN, $queen->getType());
+        $this->assertSame(Color::BLACK, $queen->getColor());
+    }
+}
