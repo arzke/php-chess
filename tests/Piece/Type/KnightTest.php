424945e3bb82befff1783310d49e263d89245411
Bruno Arsene <arsene.bruno@gmail.com>
2014-12-21 21:26:53 +0100

    Add unit tests for Knight

---
+++ tests/Piece/Type/KnightTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace tests\Piece\Type;
+
+
+use Chess\Color\Color;
+use Chess\Piece\Type\Knight;
+use Chess\Piece\Type\PieceType;
+use PHPUnit_Framework_TestCase;
+
+class KnightTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * @group knight
+     * @group piece
+     */
+    public function testInstantiateWithNullColor()
+    {
+        $knight = new Knight(null);
+
+        $this->assertSame(PieceType::KNIGHT, $knight->getType());
+        $this->assertSame(null, $knight->getColor());
+    }
+
+    /**
+     * @group knight
+     * @group piece
+     */
+    public function testInstantiateWithColor()
+    {
+        $knight = new Knight(Color::BLACK);
+
+        $this->assertSame(PieceType::KNIGHT, $knight->getType());
+        $this->assertSame(Color::BLACK, $knight->getColor());
+    }
+    
+}
