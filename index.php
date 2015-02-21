d79b3ee8c51e4d29d06ac9b76d4252a331c15190
Bruno Arsene <arsene.bruno@gmail.com>
2015-02-22 00:36:57 +0100

    Remove Board from BoardBuilder's arguments

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require "vendor/autoload.php";
 
-$boardBuilder = new \Chess\Board\Builder\BoardBuilder(new \Chess\Board\Board());
+$boardBuilder = new \Chess\Board\Builder\BoardBuilder();
 $boardDirector = new \Chess\Board\Builder\BoardDirector($boardBuilder);
 
 $chessBoard = $boardDirector->build();
