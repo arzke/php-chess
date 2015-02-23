61e456ecde89afb471beba040228ae1e908e8d73
Bruno Arsene <arsene.bruno@gmail.com>
2015-02-23 21:14:20 +0100

    Factory dynamically instantiate piece

--- src/Piece/Factory/PieceFactory.php
+++ src/Piece/Factory/PieceFactory.php
@@ -3,13 +3,7 @@
 namespace Chess\Piece\Factory;
 
 
-use Chess\Piece\Type\Bishop;
-use Chess\Piece\Type\King;
-use Chess\Piece\Type\Knight;
-use Chess\Piece\Type\Pawn;
-use Chess\Piece\Type\PieceType;
-use Chess\Piece\Type\Queen;
-use Chess\Piece\Type\Rook;
+use Chess\Piece\Type\TypeToClassMapper;
 
 class PieceFactory
 {
@@ -21,23 +15,13 @@ class PieceFactory
      */
     public function getPiece($pieceType, $color)
     {
-        if ($pieceType === PieceType::PAWN) {
-            $piece = new Pawn($color);
-        } elseif ($pieceType === PieceType::BISHOP) {
-            $piece = new Bishop($color);
-        } elseif ($pieceType === PieceType::KNIGHT) {
-            $piece = new Knight($color);
-        } elseif ($pieceType === PieceType::ROOK) {
-            $piece = new Rook($color);
-        } elseif ($pieceType === PieceType::QUEEN) {
-            $piece = new Queen($color);
-        } elseif ($pieceType === PieceType::KING) {
-            $piece = new King($color);
-        } else {
-            throw new \InvalidArgumentException("Can not build piece of unknown type: $pieceType");
+        $typeToClassMapper = new TypeToClassMapper();
+        $className = $typeToClassMapper->getClassName($pieceType);
+        if ($className) {
+            return new $className($color);
         }
 
-        return $piece;
+        throw new \InvalidArgumentException("Can not build piece of unknown type: $pieceType");
     }
 
 }
